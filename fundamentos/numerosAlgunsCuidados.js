3c8be7ba0767990a40ccf6107d03fd12bb8ad551
Odair Lopes R Jr <srlopesramos2.0@gmail.com>
2020-10-06 21:03:08 -0300

    (javascript) Numeros, strings e array

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,7 @@
+console.log(7/0); //matematica tende ao infinito
+console.log('10,2'/ 2);
+console.log('10'/2);
+console.log('Show!' * 2);
+console.log(0.1 + 0.7); //Espicificação de números e processamento
+//console.log(10.toString());
+ console.log((10.35).toString());
